6ad15f6ba4fba6141f205b343096db7b9820f877
ajarean <117534525+ajarean@users.noreply.github.com>
2026-03-16 17:27:24 -0700

    generalize build_index

--- backend/engine/keyword_search.py
+++ backend/engine/keyword_search.py
@@ -1,6 +1,8 @@
 import string
+import os
 import pandas as pd
 from rank_bm25 import BM25Okapi
+from google.cloud import bigquery
 
 class KeywordSearchEngine:
     def __init__(self):
@@ -12,9 +14,15 @@ class KeywordSearchEngine:
         text = text.translate(str.maketrans('', '', string.punctuation))
         return text.split()
     
-    # in the app, get your production data dataframe and run it with build_index first
-    def build_index(self, corpus_df):
-        self.df = corpus_df.copy()
+    # call this first
+    def build_index(self):
+        project_id = os.getenv("PROJECT_ID")
+        client = bigquery.Client(project=project_id)
+        query = f"""
+            SELECT chunk_id, repo_name, chunk_text 
+            FROM `{project_id}.searchme_dataset.embeddings_spark_50k_clean`
+        """
+        self.df = client.query(query).to_dataframe()
         self.df['chunk_text'] = self.df['chunk_text'].fillna("")
         tokenized_corpus = [self._tokenize(doc) for doc in self.df['chunk_text']]
         self.bm25 = BM25Okapi(tokenized_corpus)
@@ -23,6 +31,7 @@ class KeywordSearchEngine:
     def search(self, query, k=5):
         if self.bm25 is None:
             raise RuntimeError("BM25 index has not been built; make sure you call build_index() first.")
+        
         tokenized_query = self._tokenize(query)
         
         doc_scores = self.bm25.get_scores(tokenized_query)
