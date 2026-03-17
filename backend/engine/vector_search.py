2c3f68252b252579c93d5dd5e8214f1660385b78
Gurjot Singh <47932844+GurjotSDev@users.noreply.github.com>
2026-03-16 20:06:55 -0700

    Replace embedding model and enhance vector search
    Refactor vector search to use SentenceTransformer for embeddings and update SQL query for better results.

--- backend/engine/vector_search.py
+++ backend/engine/vector_search.py
@@ -1,29 +1,60 @@
 from google.cloud import bigquery
-import vertexai
-from vertexai.language_models import TextEmbeddingModel
+from sentence_transformers import SentenceTransformer
+import torch
 
-vertexai.init(project= "search-me-cs226", location="us-central1")
+_model_cache = {}
 
-model = TextEmbeddingModel.from_pretrained("gemini-embedding-001")
+def get_model():
+    if "all-minilm" not in _model_cache:
+        device = "cuda" if torch.cuda.is_available() else "cpu"
+        _model_cache["all-minilm"] = SentenceTransformer(
+            'all-MiniLM-L6-v2',
+            device=device
+        )
+    return _model_cache["all-minilm"]
+
+def get_embedding(text):
+    model = get_model()
+
+    vec = model.encode(
+        text,
+        normalize_embeddings=True  
+    )
+
+    return vec.tolist()
 
 client = bigquery.Client()
 
 PROJECT = "search-me-cs226"
 DATASET = "searchme_dataset"
-TABLE = "subset_50k_gemini_embeddings"
+TABLE = "embeddings_spark_50k_clean"
 
 def vector_search(query, k=10):
-    embedding = model.get_embeddings([query])[0].values
+    embedding = get_embedding(query)
     sql = f"""
-    SELECT
-        base.repo_name
-    FROM VECTOR_SEARCH(
-        TABLE `{PROJECT}.{DATASET}.{TABLE}`, 
-        'embedding', 
-        (SELECT {embedding} AS embedding),
-        top_k => {k}
+    WITH results AS (
+        SELECT
+            base.repo_name,
+            distance
+        FROM VECTOR_SEARCH(
+            TABLE `{PROJECT}.{DATASET}.{TABLE}`, 
+            'embedding', 
+            (SELECT {embedding} AS embedding),
+            top_k => {k * 10},   
+            distance_type => 'COSINE'
+        )
     )
+
+    SELECT
+        repo_name,
+        MIN(distance) AS best_distance
+    FROM results
+    GROUP BY repo_name
+    ORDER BY best_distance ASC
+    LIMIT {k}
     """
 
     df = client.query(sql).to_dataframe()
     return df["repo_name"].tolist()
+
+print(vector_search("video game engine"))
